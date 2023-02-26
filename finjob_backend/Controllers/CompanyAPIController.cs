586f1331979355aca6e8d9e879655e4441467209
Tuns <tuanmoitap@gmail.com>
2023-02-26 17:23:22 +0700

    API Response

--- finjob_backend/Controllers/CompanyAPIController.cs
+++ finjob_backend/Controllers/CompanyAPIController.cs
@@ -7,6 +7,8 @@ using finjob_backend.Repository.IRepository;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Net;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace finjob_backend.Controllers
 {
@@ -14,96 +16,154 @@ namespace finjob_backend.Controllers
     [ApiController]
     public class CompanyAPIController : ControllerBase
     {
+        protected APIResponse _response;
         private readonly ICompanyRepository _dbCompany;
         private readonly IMapper _mapper;
         public CompanyAPIController(ICompanyRepository dbCompany, IMapper mapper)
         {
             _dbCompany = dbCompany;
             _mapper = mapper;
+            this._response = new();
         }
 
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        public async Task<ActionResult<IEnumerable<CompanyDTO>>> GetCompanyList()
+        public async Task<ActionResult<APIResponse>> GetCompanyList()
         {
-            IEnumerable<Company> companyList = await _dbCompany.GetAllAsync();
-            return Ok(_mapper.Map<List<CompanyDTO>>(companyList));
+            try
+            {
+                IEnumerable<Company> companyList = await _dbCompany.GetAllAsync();
+                _response.Result = _mapper.Map<List<CompanyDTO>>(companyList);
+                _response.StatusCode = HttpStatusCode.OK;
+                return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;       
         }
 
         [HttpGet("{id:int}", Name ="GetCompany")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<ActionResult<CompanyDTO>> GetCompany(int id)
+        public async Task<ActionResult<APIResponse>> GetCompany(int id)
         {
-            if(id == 0)
+            try
             {
-                return BadRequest();
+                if (id == 0)
+                {
+                    return BadRequest();
+                }
+                var company = await _dbCompany.GetAsync(x => x.Id == id);
+                if (company == null)
+                {
+                    return NotFound();
+                }
+                _response.Result = _mapper.Map<CompanyDTO>(company);
+                _response.StatusCode = HttpStatusCode.OK;
+                return Ok(_response);
             }
-            var company = await _dbCompany.GetAsync(x => x.Id == id);
-            if (company == null)
+            catch (Exception ex)
             {
-                return NotFound();
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
             }
-            return Ok(_mapper.Map<CompanyDTO>(company));
+            return _response;
         }
 
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<ActionResult<CompanyDTO>> CreateCompany([FromBody] CompanyCreateDTO createDTO)
+        public async Task<ActionResult<APIResponse>> CreateCompany([FromBody] CompanyCreateDTO createDTO)
         {
-            if(await _dbCompany.GetAsync(u => u.Name.ToLower()== createDTO.Name.ToLower()) != null) 
+            try
             {
+                if (await _dbCompany.GetAsync(u => u.Name.ToLower() == createDTO.Name.ToLower()) != null)
+                {
 
-                ModelState.AddModelError("CustomError","Company Already exist!");
-                return BadRequest(ModelState);
+                    ModelState.AddModelError("CustomError", "Company Already exist!");
+                    return BadRequest(ModelState);
+                }
+                if (createDTO == null)
+                {
+                    return BadRequest(createDTO);
+                }
+
+                Company company = _mapper.Map<Company>(createDTO);
+
+                await _dbCompany.CreateAsync(company);
+
+                _response.Result = _mapper.Map<CompanyDTO>(company);
+                _response.StatusCode = HttpStatusCode.Created;
+                return CreatedAtRoute("GetCompany", new { id = company.Id }, _response); ;
             }
-            if (createDTO == null)
+            catch (Exception ex)
             {
-                return BadRequest(createDTO);
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
             }
-
-            Company model = _mapper.Map<Company>(createDTO);
-
-            await _dbCompany.CreateAsync(model);
-            return CreatedAtRoute("GetCompany", new { id = model.Id }, model); ;
+            return _response;
         }
 
         [HttpDelete("{id:int}", Name = "DeleteCompany")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<IActionResult> DeleteCompany(int id)
+        public async Task<ActionResult<APIResponse>> DeleteCompany(int id)
         {
-            if(id == 0)
+            try
             {
-                return BadRequest();
-            }
-            var company = await _dbCompany.GetAsync(u => u.Id == id);
-            if(company == null)
+                if (id == 0)
+                {
+                    return BadRequest();
+                }
+                var company = await _dbCompany.GetAsync(u => u.Id == id);
+                if (company == null)
+                {
+                    return NotFound();
+                }
+                await _dbCompany.RemoveAsync(company);
+                _response.StatusCode = HttpStatusCode.NoContent;
+                _response.IsSuccess = true;
+                return Ok(_response);
+            }     
+            catch (Exception ex)
             {
-                return NotFound();
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
             }
-            await _dbCompany.RemoveAsync(company);
-            return NoContent();
+            return _response;
         }
 
         [HttpPut("{id:int}", Name = "UpdateCompany")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<IActionResult> UpdateCompany(int id, [FromBody] CompanyUpdateDTO updateDTO)
+        public async Task<ActionResult<APIResponse>> UpdateCompany(int id, [FromBody] CompanyUpdateDTO updateDTO)
         {
-            if (updateDTO == null || id != updateDTO.Id)
+            try
             {
-                return BadRequest();
+                if (updateDTO == null || id != updateDTO.Id)
+                {
+                    return BadRequest();
+                }
+
+                Company model = _mapper.Map<Company>(updateDTO);
+
+                await _dbCompany.UpdateAsync(model);
+                _response.StatusCode = HttpStatusCode.NoContent;
+                _response.IsSuccess = true;
+                return Ok(_response);
             }
-
-            Company model = _mapper.Map<Company>(updateDTO);
-
-            await _dbCompany.UpdateAsync(model);
-            return NoContent();
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
         }
 
         [HttpPatch("{id:int}", Name = "UpdatePartialCompany")]
