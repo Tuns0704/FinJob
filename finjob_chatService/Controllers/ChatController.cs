61f19ce2ff8d7fdc5b2b19cb18064e098778bffe
Tuns <tuanmoitap@gmail.com>
2023-04-25 05:35:13 +0700

    Remove: SignalR

--- finjob_chatService/Controllers/ChatController.cs
+++ finjob_chatService/Controllers/ChatController.cs
@@ -1,24 +1,9 @@
-﻿using finjob_chatService.Hubs;
-using finjob_chatService.Hubs.Clients;
-using finjob_chatService.Models;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.SignalR;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace finjob_chatService.Controllers
 {
     public class ChatController : Controller
     {
-        private readonly IHubContext<ChatHub, IChatClient> _chatHub;
 
-        public ChatController(IHubContext<ChatHub, IChatClient> chatHub)
-        {
-            _chatHub = chatHub;
-        }
-
-        [HttpPost("messages")]
-        public async Task Post(ChatMessage message)
-        {
-            await _chatHub.Clients.All.ReceiveMessage(message);
-        }
     }
 }
